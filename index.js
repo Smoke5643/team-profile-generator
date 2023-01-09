8775602c93466c67ad512d8540be798b2e436025
Smoke5643 <smoke5643@gmail.com>
2023-01-09 13:41:57 -0500

    committing as stopping point for now

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const pageTemplate = require('./src/page-template')
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 const prompt = inquirer.createPromptModule();
-const teamMembers= [];
+const teamMembers = [];
 
-prompt([
+
+const managerQuestions = [
     {
         message: 'What is the manager\'s name?',
         name: 'name',
@@ -25,9 +27,105 @@ prompt([
         message: 'What is the manager\'s office number?',
         name: 'officeNumber',
     },
-]).then(({ name, id , email, officeNumber}) => {
-    const manager = new Manager (name, id, email, officeNumber);
-    teamMembers.push(manager);
-})
+]
+const engineerQuestions = [
+    {
+        message: 'What is the engineer\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the engineer\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the engineer\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the engineer\'s GitHub?',
+        name: 'officeNumber',
+    },
+]
+const internQuestions = [
+    {
+        message: 'What is the intern\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the intern\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the intern\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the intern\'s school?',
+        name: 'officeNumber',
+    },
+]
+
+const chooseEmployeeType = () => {
+
+};
+
+const selectEmployeeType = async ({ type }) => {
+    switch(type) {
+        case 'Engineer': {
+            const response = await prompt(engineerQuestions);
+            const {name, id, email, gitHub} = response;
+            const engineer = new Engineer(name, id, email, gitHub);
+            teamMembers.push(engineer);
+            break;
+        }
+        case 'Intern': {
+            const response = await prompt(internQuestions);
+            const {name, id, email, school} = response;
+            const intern = new Intern(name, id, email, school);
+            teamMembers.push(intern);
+            break;
+        }
+        case 'Manager': {
+            const response = await prompt(managerQuestions);
+            const {name, id, email, officeNumber} = response;
+            const manager = new Manager(name, id, email, officeNumber);
+            teamMembers.push(manager);
+            break;
+        }
+    }
+}
+
+const confirmMoreEmployees = ({ addEmployee }) => {
+    if (addEmployee) {
+        console.log('PROCEED');
+    } else {
+        const template = pageTemplate(teamMembers);
+        fs.writeFileSync('./dist/team.html', template)
+    };
+}
+
+const addMoreEmployees = () => {
+    return prompt({
+        message: 'Would you like to add more employees?',
+        type: 'confirm',
+        name: 'addEmployee',
+    })
+}
 
 
+prompt(managerQuestions)
+    .then(({ name, id, email, officeNumber }) => {
+        const manager = new Manager(name, id, email, officeNumber);
+        teamMembers.push(manager);
+    })
+    .then(() => {
+        return prompt({
+            message: 'Would you like to add more employees?',
+            type: 'confirm',
+            name: 'addEmployee',
+        })
+    })
+    .then(chooseEmployeeType)
+    .then(confirmMoreEmployees)
+    .then(addMoreEmployees)
+        
