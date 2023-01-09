82fe7a9b26f59b2667670bc3ed561019d5aa34e8
Smoke5643 <smoke5643@gmail.com>
2023-01-09 11:51:36 -0500

    setup manager in index.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,4 +5,7 @@ describe("Engineer", () => {
         const engineer =  new Engineer ('Guy', 1, 'smoke5643@gmail.com', 'smoke5643')
         expect(engineer.getGithub()).toEqual('Smoke5643');
     });
+    it('SHould have a role of Engineer', () => {
+        const engineer = new Engineer ('Guy', 1, 'smoke5643@gmail.com', 'Smoke5643')
+    });
 });
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-const fs = require('fs');
+
+const prompt = inquirer.createPromptModule();
+const teamMembers= [];
+
+prompt([
+    {
+        message: 'What is the manager\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the manager\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the manager\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the manager\'s office number?',
+        name: 'officeNumber',
+    },
+]).then(({ name, id , email, officeNumber}) => {
+    const manager = new Manager (name, id, email, officeNumber);
+    teamMembers.push(manager);
+})
+
 
